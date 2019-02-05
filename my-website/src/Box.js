c4a18f9d75e788cfa47a9310dd219e384e819a47
Kearnie Lin <kearnie@Kearnies-MBP.wv.cc.cmu.edu>
2019-02-05 13:23:00 -0500

    some updates

--- my-website/src/Box.js
+++ my-website/src/Box.js
@@ -7,22 +7,30 @@ class Box extends Component {
   }
 
   renderAboutInfo() {
+    const backgroundImage1 = "/a1.png";
+    const backgroundImage2 = "/a2.png";
     return (
       <div className="multibox-container">
-        <div className="content-window-main-wrapper">
+        <div 
+          className="content-window-main-wrapper"
+          style={{backgroundImage: `url(${backgroundImage1})`}}
+        >
           <div className="main-text">Hello, this is Kearnie!</div>
           <div className="main-text">I like stargazing, tea lattes with lots of foamed milk, planetariums, and sitting through movie credits.</div>
           <div className="main-text">I am currently studying computer science and art at Carnegie Mellon University, and my interests lie mainly in human-computer interaction, front-end and full-stack software development, and design.</div>
           <div className="main-text">
-            <a className="file-links" href="kearnie_resume_jul18.pdf" target="_blank">resume</a> | <a className="file-links" href="mailto:xrniel@gmail.com">xrniel@gmail.com</a>
+            <a className="file-links" href="kearnie_resume.pdf" target="_blank">resume</a> | <a className="file-links" href="mailto:xrniel@gmail.com">xrniel@gmail.com</a>
           </div>
         </div>
-        <div className="content-window-main-wrapper">
+        <div 
+          className="content-window-main-wrapper"
+          style={{backgroundImage: `url(${backgroundImage2})`}}
+        >
           <div className="main-text">Hello, this is Kearnie!</div>
           <div className="main-text">I like stargazing, tea lattes with lots of foamed milk, planetariums, and sitting through movie credits.</div>
           <div className="main-text">I am currently studying computer science and art at Carnegie Mellon University, and my interests lie mainly in human-computer interaction, front-end and full-stack software development, and design.</div>
           <div className="main-text">
-            <a className="file-links" href="kearnie_resume_jul18.pdf" target="_blank">resume</a> | <a className="file-links" href="mailto:xrniel@gmail.com">xrniel@gmail.com</a>
+            <a className="file-links" href="kearnie_resume.pdf" target="_blank">resume</a> | <a className="file-links" href="mailto:xrniel@gmail.com">xrniel@gmail.com</a>
           </div>
         </div>
       </div>
